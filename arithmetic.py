706e0aa36fc738773b2d95c5f303c033dd9f7d8e
sakshi khot <khotsakshi38@gmail.com>
2025-12-10 15:55:13 +0530

    updated add sub

---
+++ arithmetic.py
@@ -0,0 +1,5 @@
+def add(a,b):
+    print("A+B :",a+b)
+
+def sub(a,b):
+    print("A-B :",a-b)
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import arithmetic
+
+
 print("Hello World!")
+a=int(input("Enter num1 "))
+b=int(input("Enter num2 "))
+arithmetic.add(a,b)
+arithmetic.sub(a,b)
