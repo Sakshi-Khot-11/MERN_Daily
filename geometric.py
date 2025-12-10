20a1b492953c70a45fa3b72d6c608d582dc8d96a
sakshi khot <khotsakshi38@gmail.com>
2025-12-10 22:09:40 +0530

    geometric commit

---
+++ geometric.py
@@ -0,0 +1,5 @@
+def rect_area(l,b):
+    print("Area :",l*b)
+    
+def rect_perime(l,b):
+    print("Perimeter :",2*(l+b))
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import arithmetic
-
+import geometric
 
 print("Hello World!")
 a=int(input("Enter num1 "))
 b=int(input("Enter num2 "))
 arithmetic.add(a,b)
 arithmetic.sub(a,b)
+l=int(input("Enter length "))
+b=int(input("enter breadth "))
+geometric.rect_area(l,b)
+geometric.rect_perime(l,b)
